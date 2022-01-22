de50db95779e35abf269f98fd337137177804c58
jtruong128 <jtruong1@hotmail.com>
2022-01-21 21:06:46 -0600

    Initial Version

---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello World", "text/html");
+        }
+        public IActionResult Goodbye()
+        {
+            return Content("<h1>Hello World</h1>", "text/html");
+        }
+    }
+}
